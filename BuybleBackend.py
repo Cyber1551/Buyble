5f9b041c3d0172a23d03e1ce6b15ea44370e8c80
MatthewHodges <32823601+MatthewHodges@users.noreply.github.com>
2020-02-07 23:21:49 -0600

    Create BuybleBackend.py
    Update and insert method added

---
+++ BuybleBackend.py
@@ -0,0 +1,28 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+
+
+client = MongoClient("mongodb+srv://dbUser:beholdtheWineCaves@cluster0-itr0f.mongodb.net/test?retryWrites=true&w=majority")
+db = client['stores']
+#pprint.pprint(db.collection_names())
+
+collection = db['storeOne']
+#pprint.pprint(collection.distinct("_id"))
+#collection.insert_one({"x":1})
+#collection.update_one({"x": 1},{"$inc":{"x":3}})
+
+#doc = collection.find_one({"Name" : "FirstDummy"})
+#pprint.pprint(doc)
+
+
+def addPurchase(docId, collection, quantity):
+    col = db[collection]
+    #doc = col.find_one({"_id" : docId})
+    col.update_one({"_id":docId},{"$inc":{"quantity":quantity}})
+
+def insert(date, weather, quantity, product, store):
+    col = db[store]
+    col.insert_one({"product":product, "quantity":quantity,
+                    "weather": weather, "date":date})
+
